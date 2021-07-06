e179a82e8bf741e190b6b5ed8935b48acfc7fd51
SamLiu666 <168315916@qq.com>
2021-07-07 00:25:33 +0800

    0707

---
+++ programing_language_tutor/python/data_structure.py
@@ -0,0 +1,12 @@
+'''
+Description: 
+Version: 2.0
+Autor: lxp
+Date: 2021-07-06 23:37:34
+LastEditors: lxp
+LastEditTime: 2021-07-06 23:37:56
+'''
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
---
+++ programing_language_tutor/python/use_.py
@@ -0,0 +1,15 @@
+'''
+Description: 
+Version: 2.0
+Autor: lxp
+Date: 2021-07-06 23:37:42
+LastEditors: lxp
+LastEditTime: 2021-07-06 23:38:55
+'''
+from data_structure import ListNode
+
+temp_cur.next = ListNode(3, None)
+temp_cur.next = ListNode("3", None)
+temp_cur.next = ListNode("null", None)
+temp_cur.next = ListNode("None", None)
+print("Done")
