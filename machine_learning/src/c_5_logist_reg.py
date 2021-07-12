eabd542a5e36533082843d18d032a4e186985bc8
SamLiu666 <168315916@qq.com>
2021-07-12 23:06:32 +0800

    0712

--- machine_learning/src/c_5_logist_reg.py
+++ machine_learning/src/c_5_logist_reg.py
@@ -4,7 +4,7 @@ Version: 2.0
 Autor: lxp
 Date: 2021-07-04 22:45:13
 LastEditors: lxp
-LastEditTime: 2021-07-04 22:48:27
+LastEditTime: 2021-07-10 10:08:11
 '''
 import numpy as np
 import pandas as pd
@@ -67,3 +67,54 @@ class LogisticRegression_numpy(object):
             labels.append(self.predict_(x))
 
         return labels
+
+
+class Network_reg_np(object):
+    def __init__(self, num_of_weights):
+        # 随机产生w的初始值
+        # 为了保持程序每次运行结果的一致性，
+        # 此处设置固定的随机数种子
+        np.random.seed(0)
+        self.w = np.random.randn(num_of_weights, 1)
+        self.b = 0.
+
+    def forward(self, x):
+        z = np.dot(x, self.w) + self.b
+        return z
+
+    def loss(self, z, y):
+        error = z - y
+        cost = error * error
+        cost = np.mean(cost)
+        return cost
+
+    def gradient(self, x, y):
+        z = self.forward(x)
+        gradient_w = (z - y) * x
+        # axis = 0 表示把每一行做相加然后再除以总的行数
+        gradient_w = np.mean(gradient_w, axis=0)
+        gradient_w = gradient_w[:, np.newaxis]
+        gradient_b = (z - y)
+        gradient_b = np.mean(gradient_b)
+        return gradient_w, gradient_b
+
+    def update(self, gradient_w, gradient_b, eta=0.01):
+        self.w = self.w - eta * gradient_w
+        self.b = self.b - eta * gradient_b
+
+    def fit(self, x, y, iterations=100, eta=0.01, verbose=False):
+        losses = []
+        for i in range(iterations):
+            z = self.forward(x)
+            L = self.loss(z, y)
+            gradient_w, gradient_b = self.gradient(x, y)
+            self.update(gradient_w, gradient_b, eta)
+            losses.append(L)
+            if verbose:
+                if (i + 1) % 100 == 0:
+                    print('iter {}, loss {}'.format(i, L))
+        return losses
+    
+    def predict(self, x):
+        z = self.w * x + self.b
+        
