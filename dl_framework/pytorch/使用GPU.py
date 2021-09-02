e24722c1b13d32e745675c4d9b062135c000e38f
SamLiu666 <168315916@qq.com>
2021-09-02 14:15:48 +0800

    c++

--- dl_framework/pytorch/使用GPU.py
+++ dl_framework/pytorch/使用GPU.py
@@ -4,5 +4,7 @@ Version: 2.0
 Autor: lxp
 Date: 2021-07-26 21:53:40
 LastEditors: lxp
-LastEditTime: 2021-07-26 21:53:45
+LastEditTime: 2021-09-02 08:56:01
 '''
+import torch
+print(torch.cuda.is_available())
