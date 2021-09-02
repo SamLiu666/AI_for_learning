7069a618d0ce315509dc667df3221f06f5a2dcb4
SamLiu666 <168315916@qq.com>
2021-09-02 23:50:10 +0800

    c++

---
+++ programing_language_tutor/c++/ControlFlow/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: lxp
+ * @Date: 2021-09-02 22:14:24
+ * @LastEditors: lxp
+ * @LastEditTime: 2021-09-02 22:18:06
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+    int hour24, minute24, second24;
+    char temp;
+
+    cout << "Input a time in 24-hour format: ";
+    cin >> hour24 >> temp >> 
+}
