2db7ecf729c254cd8cbfe0a4b7bd81a3f8859792
SamLiu666 <168315916@qq.com>
2021-10-19 21:45:25 +0800

    book

--- temp.cpp
+++ temp.cpp
@@ -4,12 +4,17 @@
  * @Autor: lxp
  * @Date: 2021-09-02 14:29:30
  * @LastEditors: lxp
- * @LastEditTime: 2021-10-16 20:40:05
+ * @LastEditTime: 2021-10-19 17:44:48
  */
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
-    return -1;
+    int i, &ri = i;
+    i = 5; ri = 10;
+    std::cout << i << " " << ri << std::endl;
+    int *p = &i;
+    cout << "p: " << p << " *p " << *p << std::endl;
+    return 0;
 }
