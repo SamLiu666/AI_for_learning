a4a13a22b6c6a92f5ebdddbc814caa28f5e71189
SamLiu666 <168315916@qq.com>
2021-12-02 13:02:26 +0800

    d

---
+++ gitAuto.py
@@ -0,0 +1,18 @@
+'''
+Description: 
+Version: 2.0
+Autor: lxp
+Date: 2021-12-02 12:51:15
+LastEditors: lxp
+LastEditTime: 2021-12-02 13:00:40
+'''
+from os import system as git_command
+
+git_command('git status')
+git_commit_text = input("Enter Push Record Content: ")
+print("git_commit_text: ", git_commit_text)
+git_command('git add .')
+git_commit = "git commit -m " + git_commit_text
+git_command(git_commit)
+git_command('git status')
+# git_command('git push origin main')
